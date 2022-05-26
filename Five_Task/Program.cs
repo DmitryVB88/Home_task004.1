2f61573308af82ad4f6fcf4f698963d858e7c2a3
Dima <dima.brashovyan@mail.ru>
2022-05-26 09:50:18 +0300

    произведение парных элементов

---
+++ Five_Task/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найдите произведение пар в массиве. 
+Console.Clear();
+int arrSize = int.Parse(Console.ReadLine());
+int[] array = new int[arrSize];
+for(int i = 0; i < array.Length; i++) array[i] = new Random().Next(0, 15);
+for(int i = 0; i < array.Length; i++) 
+Console.Write($"{array[i] + " "}");
+Console.WriteLine();
+
+int prod = 0;
+for(int i = 0; i < array.Length; i++) 
+{ 
+    // prod = array[i] * array.Length;  
+    // Console.Write($"{prod + " "}");
+    for(int i = 0; i > array.Length; i++)
+    Console.Write($"{array[array.Length] + " "}");
+    break;
+}
+          
