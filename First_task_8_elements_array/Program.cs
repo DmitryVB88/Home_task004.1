c13b5a262ead3294dc2d5d5e2a4344300349858b
Dima <dima.brashovyan@mail.ru>
2022-05-27 13:31:12 +0300

    массив из 8-ми элементов.

---
+++ First_task_8_elements_array/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задать массив из 8 элементов.
+int arrSize = int.Parse(Console.WriteLine());
+int[] array = [arrSize];
+for(int i = 0; i < array.Length; i++) 
+array[i] = new Random().Next(0,2);
+for(int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i] + " "}");
+
+}
+
+p = array[I]*array[arry.Length-i-1] - решение перемножение пар. 
+
