17f505f846241aa0dd89d87870fab900981fbb25
Dima <dima.brashovyan@mail.ru>
2022-05-27 13:30:00 +0300

    Перемножение пар массива

--- Five_Task/Program.cs
+++ Five_Task/Program.cs
@@ -1,19 +1,28 @@
 ﻿// Найдите произведение пар в массиве. 
 Console.Clear();
-int arrSize = int.Parse(Console.ReadLine());
-int[] array = new int[arrSize];
-for(int i = 0; i < array.Length; i++) array[i] = new Random().Next(0, 15);
-for(int i = 0; i < array.Length; i++) 
-Console.Write($"{array[i] + " "}");
+// int arrSize = int.Parse(Console.ReadLine());
+int[] array = new int[12];
+int p = 0;
+for(int i = 0; i < array.Length; i++) array[i] = new Random().Next(1, 10);
+for(int i = 0; i < array.Length; i++) Console.Write($"{array[i] + " "}");
 Console.WriteLine();
-
-int prod = 0;
-for(int i = 0; i < array.Length; i++) 
-{ 
-    // prod = array[i] * array.Length;  
-    // Console.Write($"{prod + " "}");
-    for(int i = 0; i > array.Length; i++)
-    Console.Write($"{array[array.Length] + " "}");
-    break;
+for(int i = 0; i < 6; i++)
+{
+    p = array[i] * array[array.Length - 1 - i];
+    
+    Console.Write($"{p + " "}");
 }
+
+// Console.Write(p);
+// Console.WriteLine();
+
+// int p = 0;
+// for(int i = 0; i < array.Length; i++) 
+// { 
+//     // prod = array[i] * array.Length;  
+//     // Console.Write($"{prod + " "}");
+//     // for(int i = 0; i > array.Length; i++)
+//     p = array[array.Length - 1];
+    
+// } Console.Write(p);
           
